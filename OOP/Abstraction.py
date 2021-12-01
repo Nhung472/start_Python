dc622b0ebd64b813d259282b195733319f1093bd
lethinhung9819@gmail.com <lethinhung9819@gmail.com>
2021-12-01 21:10:05 +0700

    done

--- OOP/Abstraction.py
+++ OOP/Abstraction.py
@@ -1,7 +1,6 @@
 # Tính trừu tượng
 #có thể ẩn tất cả dữ liệu hoặc quy trình không liên quan
 #khi có thay đổi hoạt động bên trong 1 hàm thì cũng không ảnh hưởng tới việc bên khác gọi hàm này
-
 class Coffee:
     def __init__(self):
         pass
