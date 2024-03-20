701f3c2a2ea84e204368614e32399199d7b13daa
Lê Nhung <89861867+Nhung472@users.noreply.github.com>
2024-03-20 20:06:55 +0700

    string

---
+++ cour/stringgg.py
@@ -0,0 +1,22 @@
+a = print("Hello " + "world")
+
+greeting1 = "hello "
+gretting2 = "world"
+
+b = print(greeting1 + gretting2)
+
+danger = "Danger! "
+c = print(danger * 3)
+
+quote = "\"Thank you\""
+print(quote)
+
+gretting = "Good day, \nsir."
+print(gretting)
+
+newline = "\\n represents a new line."
+print(newline)
+
+python = "Python"
+for letter in python:
+    print(letter + 'ut')
